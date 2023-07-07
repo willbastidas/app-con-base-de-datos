1e0fccc5d121d1455d8b093a8b3eeca33524f492
willbastidas <willkergaciab@gmail.com>
2023-07-07 17:00:21 -0400

    7/7/2023

--- screen/detailsnote.js
+++ screen/detailsnote.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { Text, View } from 'react-native'
 
-export default function createNote () {
+export default function detailsNote () {
     return (
       <View>
         <Text> textInComponent </Text>
---
+++ screen/notes.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { Text, View } from 'react-native'
+
+export default function notes () {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  
+}
+const styles = StyleSheet.creat({
+
+    
+})
